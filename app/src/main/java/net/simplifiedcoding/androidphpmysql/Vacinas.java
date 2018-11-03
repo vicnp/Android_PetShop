71f613fdbc31e54bb1aa898aba627d315d953d43
vicnp <vicnp9@hotmail.com>
2018-11-02 23:48:09 -0300

    Novas Telas

---
+++ app/src/main/java/net/simplifiedcoding/androidphpmysql/Vacinas.java
@@ -0,0 +1,13 @@
+package net.simplifiedcoding.androidphpmysql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vacinas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacinas);
+    }
+}
---
+++ app/src/main/java/net/simplifiedcoding/androidphpmysql/dadosConsultas.java
@@ -0,0 +1,13 @@
+package net.simplifiedcoding.androidphpmysql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dadosConsultas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_consultas);
+    }
+}
